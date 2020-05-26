2fb2b8014fb4ae9cddc55c6daf96f9f6a78790bc
Jay Mohile <jamohile@gmail.com>
2020-05-26 13:46:54 -0400

    Add dynamically rendered projects.

--- portfolio/src/main/webapp/js/projects.js
+++ portfolio/src/main/webapp/js/projects.js
@@ -2,6 +2,10 @@ const tagNames = ["Personal", "Google", "JS", "C++"];
 const tagTemplate = document.querySelector("#tag-template");
 const tagContainer = document.querySelector("#tags-list");
 
+const projectObjects = [{name: "Recall", description: "Hospital Scheduling", tags: ["Personal"]}];
+const projectTemplate = document.querySelector("#project-template");
+const projectContainer = document.querySelector("#projects-list");
+
 /**
  * Tags help group projects.
  * The Tag class is used to display (and keep state) for
@@ -66,6 +70,67 @@ class Tag {
         }
     }
 }
+
+class Project {
+    /** All projects currently being displayed, by name. */
+    static projects = new Map();
+
+    /**
+     * @param {string} name
+     * @param {string} description
+     * @param {string[]} tags
+     */
+    constructor(name, description, tags){
+        /** Get root HTML node */
+        this.node = projectTemplate.content.cloneNode(true).querySelector(".project");
+
+        /** @private @const {string} */
+        this.name = name;
+        /** @private @const {string} */
+        this.description = description;
+        /** @private @const {string[]} */
+        this.tags = tags;
+
+        this.render();
+        
+        /** Add to UI */
+        projectContainer.appendChild(this.node);
+    }
+
+    /**
+     * Set all child nodes and their visual properties based on state.
+     * @return {undefined}
+     */
+    render(){
+        this.node.querySelector(".name").innerHTML = this.name;
+        this.node.querySelector(".description").innerHTML = this.description;
+
+        /** Show a list of all tags this project has. */
+        const tagsContainer = this.node.querySelector(".tags");
+        const tagTemplate = tagsContainer.querySelector(".project-tag-template");
+        /** Clear any existing tags from UI. */
+        tagsContainer.innerHTML = "";
+        /** Add all tags to UI */
+        for(let tagName of this.tags){
+            const tagNode = tagTemplate.content.cloneNode(true).querySelector(".tag");
+            tagNode.innerHTML = tagName;
+            tagsContainer.appendChild(tagNode);
+        }
+    }
+
+    /** Add all projects to the UI, replacing all existing. */
+    static populateAll(){
+        projectContainer.innerHTML = "";
+        Project.projects.clear();
+
+        for(let {name, description, tags} of projectObjects){
+            Project.projects.set(name, new Project(name, description, tags));
+        }
+    }
+}
+
+/** Add all projects to screen. */
+Project.populateAll();
 
 /** Initially add all tags to screen, unselected. */
 Tag.populateAll();
