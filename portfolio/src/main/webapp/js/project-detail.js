3c5abc26950343a58d1f0d225b245c88b05a634f
Jay Mohile <jamohile@gmail.com>
2020-05-29 10:52:27 -0400

    Add logic to display project links.

--- portfolio/src/main/webapp/js/project-detail.js
+++ portfolio/src/main/webapp/js/project-detail.js
@@ -8,7 +8,8 @@ const projectId = new URLSearchParams(location.search).get("id");
 
 /**
  * All data about the current project.
- * @type {{id: string, name: string, description: string, detail: string[], tags: string[]}}
+ * @typedef {{name: string, href: string}} ProjectLink
+ * @type {{id: string, name: string, description: string, detail: string[], tags: string[], links: ProjectLink[]}}
  */
 const project = projectsObject[projectId];
 
@@ -26,4 +27,28 @@ for(let detail of project.detail){
     const detailNode = document.createElement("li");
     detailNode.innerHTML = detail;
     detailListNode.appendChild(detailNode);
+}
+
+/** Similar to the list of details, create a list of links but only if present. */
+const linkSectionNode = document.querySelector("#project-links");
+if (project.links && project.links.length > 0) {
+    linkSectionNode.classList.remove("hidden");
+
+    const linkListNode = linkSectionNode.querySelector("#links");
+
+    for(let link of project.links){
+        const {name, href} = link;
+
+        /** Create link element. */
+        const linkNode = document.createElement("a");
+        linkNode.href = href;
+        linkNode.innerHTML = name;
+
+        /** Add link to UI. */
+        const listElementNode = document.createElement("li");
+        listElementNode.appendChild(linkNode);
+        linkListNode.appendChild(listElementNode);
+    }
+} else{
+    linkSectionNode.classList.add("hidden");
 }
